653c8e3632d24fe1818a9ededb38a86c234ca886
OmitheOG <97448506+OmitheOG@users.noreply.github.com>
2022-01-10 13:53:12 +0100

    Update Log In.py

--- Log In.py
+++ Log In.py
@@ -1,15 +1,13 @@
-username = input( "Username: ")
-passwort = input("Passwort eingeben: ")
+username = input( "Username: ")  # Hier gibt man den Nutzernamen ein
+passwort = input("Passwort eingeben: ") # Hier gibt man das Passwort ein
 
-if username == "Omar Azimi":
+if username == "Omar Azimi": # Wenn der Nutzername = "" ist, soll man als nächstes das Passwort eingeben.
     
-    if passwort == "Hallo" :
+    if passwort == "Hallo" : # Wenn das Passwort = "" ist kommt ein string, wenn es falsch ist, kommt auch ein String
         print ("Hallo du Noob")
     else:
         print ("You are a DEAD man DAWG, I'LL get ya! Im gonna shoot you down dawg!!!") 
 
-else:
-    print ("Ungültig")
-
+else: print (" Ungültig") # Wenn man den Nutzernamen falsch eingegeben hat, kommt dieser String
 
 
