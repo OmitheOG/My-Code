d7540f91e48432d8f52ff3ee99e81579ba0640ac
OmitheOG <97448506+OmitheOG@users.noreply.github.com>
2022-01-10 13:47:04 +0100

    Add files via upload

---
+++ Log In.py
@@ -0,0 +1,15 @@
+username = input( "Username: ")
+passwort = input("Passwort eingeben: ")
+
+if username == "Omar Azimi":
+    
+    if passwort == "Hallo" :
+        print ("Hallo du Noob")
+    else:
+        print ("You are a DEAD man DAWG, I'LL get ya! Im gonna shoot you down dawg!!!") 
+
+else:
+    print ("Ungültig")
+
+
+
